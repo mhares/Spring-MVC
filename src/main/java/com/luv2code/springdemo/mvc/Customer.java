69a04f9e6bb997779fb69a7d1de32657fd399277
unknown <Mostafa Hares>
2021-05-29 18:05:45 +0200

    Max and Min validation

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,6 +11,9 @@ public class Customer {
 	@NotNull(message="Is Required")
 	@Size(min=1,message="Is Required")
 	private String lastName;
+	@Min(value=0,message="Must be equal or greater than zero")
+	@Max(value=10,message="Must be equal or less than Ten")
+	private int freePasses;
 
 	public String getFirstName() {
 		return firstName;
@@ -24,6 +29,14 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 	
 	
